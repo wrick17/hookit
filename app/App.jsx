fa2903aefedc7fb20fdcded0e28bfc13b6534938
Pratyush Poddar <pratyush.poddar@ymedialabs.com>
2016-09-21 13:52:53 +0530

    move code to header

--- app/App.jsx
+++ app/App.jsx
@@ -7,28 +7,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="app">
-        {/* Uses a transparent header that draws on top of the layout's background */}
-        <div style={{height: '100vh', position: 'relative'}}>
-          <Layout>
-            <Header transparent title="Title" style={{color: 'white', height: '100px', background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
-              <Navigation>
-                <a href="">Link 1</a>
-                <a href="">Link 1</a>
-                <a href="">Link 1</a>
-                <a href="">Link 1</a>
-              </Navigation>
-            </Header>
-            <Drawer title="Title">
-              <Navigation>
-                <a href="">Link 2</a>
-                <a href="">Link 2</a>
-                <a href="">Link 2</a>
-                <a href="">Link 2</a>
-              </Navigation>
-            </Drawer>
-            <Content />
-          </Layout>
-        </div>
+        yo
       </div>
     );
   }
